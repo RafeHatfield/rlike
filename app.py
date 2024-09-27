d76c26d9f81a2602f2236ca0505e08529141feec
Rafe Hatfield <rafe.hatfield@gmail.com>
2024-09-27 10:27:56 -0700

    messing with linting

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
-print("Hello, World!")
+"""Module printing Hello World."""
+
+def main():
+    """Function printing Hello World."""
+    print("Hello, World!")
+
+if __name__ == "__main__":
+    main()
